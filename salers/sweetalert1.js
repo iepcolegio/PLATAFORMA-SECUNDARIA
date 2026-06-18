4d59a977507e20262051b6d4ff4b2286486c106c
iepcolegio <rosario20125asia@gmail.com>
2026-06-18 02:34:13 -0500

    Add files via upload

---
+++ salers/sweetalert1.js
@@ -0,0 +1,28 @@
+Swal.fire({
+  title: "<strong>GUARDO MIS TRABAJOS EN:</strong>",
+  html: `
+    
+    <a href="TRABJOS (file://DESKTOP-K01EHQL/Users/DOCENTE-20125/Documents/TRABJOS)
+" autofocus>CARPETA DE 1 GRADO</a>,
+    <BR>
+     <a href="https://drive.google.com/drive/folders/1AFIsF9OooznC1M5YQIcqPnF8mDFwmoRN" autofocus>CARPETA DE 2 GRADO</a>,
+    <BR>
+    <a href="https://drive.google.com/drive/folders/1AFIsF9OooznC1M5YQIcqPnF8mDFwmoRN" autofocus>CARPETA DE 3 GRADO</a>,
+    <BR>
+    <a href="https://drive.google.com/drive/folders/1AFIsF9OooznC1M5YQIcqPnF8mDFwmoRN" autofocus>CARPETA DE 4 GRADO</a>,
+    <BR>
+    <a href="https://drive.google.com/drive/folders/1AFIsF9OooznC1M5YQIcqPnF8mDFwmoRN" autofocus>CARPETA DE 5 GRADO</a>,
+    
+  `,
+  showCloseButton: false,
+  showCancelButton: false,
+  focusConfirm: false,
+  confirmButtonText: `
+    <i class="fa fa-thumbs-up"></i> CERRAR
+  `,
+ 
+  cancelButtonText: `
+    <i class="fa fa-thumbs-down"></i>
+  `,
+  
+});
